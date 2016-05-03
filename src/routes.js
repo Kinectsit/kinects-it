a845e3c68a3a96818545cf5861e6fd833c1f5c9c
Bucko <bucko.perley@gmail.com>
2016-05-02 17:21:04 -0700

    (feat) Adding first routes - Index Route with some home components - splitting off views and components (so there is a home component, and then a landing page view within it for users not logged in)

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import Home from './components/Home';
 
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+  </Route>
 );
---
+++ src/views/LandingPageView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import {Link} from 'react-router';
+
+export const HomeView = () => (
+  <div>
+    <h1>Here's how it works</h1>
+  </div>
+);
