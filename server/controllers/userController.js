52f667b55bb469be23cfef2813fcd898bc7b078e
Krista Moroder <kmoroder@gmail.com>
2016-05-03 17:22:59 -0700

    (Feat) Combine user model with controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,11 @@
 const User = require('../models/userModel.js');
 const logger = require('../config/logger.js');
 
-exports.signIn = (req, res) => {
-  logger.info('Log a message');
-  User.test();
-  res.json(
-    { msg: 'Test JSON Response' }
-  );
+exports.signIn = (newUser) => {
+  User.create(newUser)
+    .then((res) => {
+      logger.info(res.dataValues);
+    })
+    .catch((err) => console.log(err));
 };
 
