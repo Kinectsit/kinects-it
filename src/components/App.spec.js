696ebdb99c8bfaf81e7fc4ca701d2155c79070ad
Bucko <bucko.perley@gmail.com>
2016-05-05 12:10:55 -0700

    (tests) two passing client side tests to see if components render

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from 'enzyme';
 import { expect } from 'chai';
-import sinon from 'sinon';
-import App from './App';
-import { Link } from 'react-router';
+// import sinon from 'sinon';
+import { App } from './App';
+// import { Link } from 'react-router';
 
 describe('<App />', () => {
   it('should have a navbar', () => {
-    const wrapper = render(<App />, {});
+    const wrapper = render(<App />);
     const actual = wrapper.find('ul').text();
     const expected = 'Home';
-    expect(actual).to.equal(expected);
+    expect(actual).to.contain(expected);
   });
 });
 
