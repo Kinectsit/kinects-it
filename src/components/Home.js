2a20d62c52f8764a3cee721eb40c08776904c583
Bryan Newby <brnewby602@gmail.com>
2016-05-09 13:22:04 -0700

    (feat) Add icon to home page, remove passport

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,12 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 export const Home = () => (
   <div>
-    <h1>Welcome to Kinects.It!</h1>
+    <h1>Welcome to Kinects.It Everything is Awesome!</h1>
     <div>
       <h2 id="how-it-works">Here's how it works</h2>
       <RaisedButton label="Get Started" />
+
+      <i className="material-icons">arrow_downward</i>
     </div>
   </div>
 );
