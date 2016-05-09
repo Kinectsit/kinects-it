fc3ae45d1ee6895fe115d886e5b8d2112ec21297
Bryan Newby <brnewby602@gmail.com>
2016-05-09 16:17:17 -0700

    (feat) add home styling, comment out passport from server.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 
 export const Home = () => (
   <div>
-    <h1>Welcome to Kinects.It Everything is Awesome!</h1>
-    <div>
-      <h2 id="how-it-works">Here's how it works</h2>
-      <RaisedButton label="Get Started" />
+    <Card>
+      <CardMedia
+        overlay={
+          <div>
+            <CardTitle title="Bringing the Internet of Things into your home." />
+            <CardActions>
+              <FlatButton label="Get Started" linkButton="true" href="/login" />
+            </CardActions></div>}
+      >
+        <img src="/../assets/tech.jpg" role="presentation" />
+      </CardMedia>
+    </Card>
 
-      <i className="material-icons">arrow_downward</i>
+    <div style={{ textAlign: 'center' }}>
+      <h2 id="how-it-works">Here's how it works</h2>
+      <div>
+        <i className="material-icons">graphic_eq</i>  
+      </div>
+      <div>
+        Attach a device to your home.
+      </div>
+      <div>
+        <i className="material-icons">arrow_downward</i>
+      </div>
+      <div>
+        <i className="material-icons">signal_wifi_4_bar</i>
+      </div>
+      <div>
+        Connect the device
+      </div>
+      <div>
+        <i className="material-icons">arrow_downward</i>
+      </div>
+      <div>
+        <i className="material-icons">person_add</i>
+      </div>
+      <div>
+        Guests are added to your domicile.
+      </div>
+      <div>
+        <i className="material-icons">arrow_downward</i>
+      </div>
+      <div>
+        <i className="material-icons">insert_emoticon</i>
+      </div>
+      <div>
+        That's it!
+      </div>
+      <RaisedButton label="Get Started" primary="true" linkButton="true" href="/login" />
     </div>
   </div>
 );
