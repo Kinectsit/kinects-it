2b15117e1d1ad0ecb7a463478f7323eb5effdc8e
Bucko <bucko.perley@gmail.com>
2016-05-10 12:19:40 -0700

    (feat) signup form authenticates and saves user to database TODO: fix pg query to handle errors correctly

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -3,9 +3,22 @@ const router = require('express').Router();
 const userController = require('../controllers/userController.js');
 
 module.exports = (app, passport) => {
-  app.route('/api/v1/users').post(passport.authenticate('local-signup', {
-      successRedirect : '/', // redirect to the secure profile section
-      failureRedirect : '/signup', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
+  app.route('/api/v1/users').post((req, res, next) => {
+    passport.authenticate('local-signup', (err, user, info) => {
+      if (err) return next(err);
+      if (!user) {
+          return res.json(403, {
+              message: "no user found"
+          });
+      }
+
+      // Manually establish the session...
+      req.login(user, function(err) {
+          if (err) return next(err);
+          return res.json({
+              message: 'user authenticated',
+          });
+      });
+    })(req, res, next);
+  });
 };
