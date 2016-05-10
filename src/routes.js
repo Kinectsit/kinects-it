9c9aed8fc47ecdf9f5486adf45bf0cff9475e76d
Bucko <bucko.perley@gmail.com>
2016-05-09 17:46:40 -0700

    rebase with new front-end changes and fixed some errors from merge

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import App from './components/App';
+import { App } from './components/App';
 import { Home } from './components/Home';
 import LoginPage from './containers/LoginPage';
 import SignupPage from './containers/SignupPage';
