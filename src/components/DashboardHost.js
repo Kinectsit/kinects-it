fbc7eb4a60372ad55ed1a82e269f1ca1cee729f7
Krista Moroder <kmoroder@gmail.com>
2016-05-09 20:22:58 -0700

    (Feat) Fixes dashboard host

--- src/components/DashboardHost.js
+++ src/components/DashboardHost.js
@@ -6,11 +6,12 @@ export const DashboardHost = (props) => (
   <div>
     <h1>Dashboard Host</h1>
     <DeviceAddButton />
-    <DeviceList appState={props.appState} actions={props.actions}/>
+    <DeviceList appState={props.appState} actions={props.actions} />
   </div>
 );
 
 DashboardHost.propTypes = {
   appState: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
 };
 
