621d8387445afe0f5e87b59b8b789020ca4ff070
Krista Moroder <kmoroder@gmail.com>
2016-05-14 15:36:05 -0700

    Make container name match route

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 import { App } from './components/App';
 import { Home } from './components/Home';
-import DemoPage from './containers/Demo';
+import Demo from './containers/Demo';
 import LoginPage from './containers/LoginPage';
 import { SignupPage } from './components/SignupPage';
 import DashboardPage from './containers/DashboardPage';
@@ -19,7 +19,7 @@ import { requireAuthentication } from './containers/requireAuthentication';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="/demo" component={DemoPage} />
+    <Route path="/demo" component={Demo} />
     <Route path="/login" component={LoginPage} />
     <Route path="/signup" component={SignupPage} />
     <Route path="/dashboard" component={requireAuthentication(DashboardPage)} />
