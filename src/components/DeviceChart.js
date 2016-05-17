e3adf790c0b87f32b83f9b1199268ab8374c9eb8
Krista Moroder <kmoroder@gmail.com>
2016-05-17 16:29:40 -0700

    Add data to bar chart

--- src/components/DeviceChart.js
+++ src/components/DeviceChart.js
@@ -2,31 +2,47 @@ import React, { PropTypes } from 'react';
 import rd3 from 'rd3';
 const BarChart = rd3.BarChart;
 
-const barData = [
-  {
-    name: 'Series A',
-    values: [
-      { x: 1, y: 91 },
-      { x: 2, y: 290 },
-      { x: 3, y: 30 },
-      { x: 4, y: 50 },
-      { x: 5, y: 90 },
-      { x: 6, y: 100 },
-    ],
-  },
-];
 
 export class DeviceChart extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      barData: [
+        {
+          values: [
+            { x: 1, y: 91 },
+            { x: 2, y: 290 },
+            { x: 3, y: 30 },
+          ],
+        },
+      ],
+    };
+  }
+
   componentWillMount() {
-    console.log(JSON.stringify(this.props.transactions));
+    const length = Math.min(this.props.transactions.length, 6);
+    const values = [];
+    for (let i = 0; i < length; i++) {
+      const newObj = {};
+      newObj.x = this.props.transactions[i].timestamp;
+      newObj.y = this.props.transactions[i].amountspent;
+      values.push(newObj);
+    }
+    this.setState({
+      barData: [
+        {
+          values,
+        },
+      ],
+    });
   }
 
   render() {
     return (
       <div>
         <BarChart
-          data={barData}
+          data={this.state.barData}
           width={700}
           height={200}
           fill={'#3182bd'}
